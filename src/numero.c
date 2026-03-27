32c39d82fa8848d74598842c686e421a384e35d8
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-03-27 09:08:56 -0300

    .

--- src/numero.c
+++ src/numero.c
@@ -1,19 +1,49 @@
 #include <stdio.h>
-#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+int guessNumber(){
+
+    int magic, adv;
+    
+    srand(time(NULL));
+    magic = rand() % 100;
+    
+    printf("Qual numero estou pensando (%d)?\n", magic);
+    scanf("%d", &adv);
+    
+    if (adv == magic){
+        printf("sao iguais");
+    } else {
+        printf("nao sao iguais");
+    }
+
+    return 0;
+}
+
+int maxValue(){
+
+    int a, b;
+
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &a, &b);
+
+    if (a == b){
+        printf("%d e %d sao iguais", a, b);
+    } else if (a > b){
+        printf("valor %d maior que %d", a, b);
+    } else {
+        printf("%d e maior que %d", b, a);
+    }
+
+    return 0;
+}
 
 int main(){
-    setlocale(LC_ALL, "Portuguese_Brazil.1252");
 
-    int n;
-
-    while (1){
-        printf("Insira um numero: ");
-        scanf("%d", &n);
-
-        if (n % 2 == 0){printf("Numero par\n");} 
-            else { printf("Numero ímpar\n");}
-
-    }
+    // guessNumber();
+    // printf("\n\n");
+    maxValue();
 
     return 0;
 }
