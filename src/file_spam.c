a8c32efd55778e6f8f8e1fa384dad1e37c660001
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-04-16 10:54:28 -0300

    add: src/lista-02c | update: lista-02c/exec01.c finished

--- src/file_spam.c
+++ src/file_spam.c
@@ -3,22 +3,28 @@
 int main() {
     FILE *fptr;
 
-    for (int i = 1; i <= 6; i++) {
+    for (int i = 2; i <= 8; i++) {
         char filename[150];
 
         printf("\nSalvando outro arquivo...\n");
 
         snprintf(filename, sizeof(filename),
-                 "C:/Users/laboratorio/alg_prog/alg_prog/src/lista-02b/exec0%d.c", i);
+                 "C:/Users/laboratorio/alg_prog/alg_prog/src/lista-02c/exec0%d.c", i);
 
         printf("%s\n", filename);
 
         fptr = fopen(filename, "w");
-
+        
         if (fptr == NULL) {
             printf("Erro ao criar o arquivo!\n");
             continue;
         }
+        
+        fprintf(fptr, "#include <stdio.h>");
+        fprintf(fptr, "\nint main(){");
+        fprintf(fptr, "\n\tprintf(\"Hello World\");");
+        fprintf(fptr, "\n\treturn 0;");
+        fprintf(fptr, "\n}");
 
         printf("Arquivo %s foi salvo!\n", filename);
 
