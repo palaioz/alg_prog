28bdbc0109b6638af52cf1cfe8e6e7e02ec69839
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-05-18 09:13:17 -0300

    m

--- src/listas/lista-03/exec8.c
+++ src/listas/lista-03/exec8.c
@@ -1,7 +1,23 @@
 // 8. Ler o salário de 10 funcionários e calcular a média de salários.
 
 #include <stdio.h>
+
 int main(){
-	printf("Hello World");
+	float soma_salario;
+	float media;
+	float salario;
+
+	printf("Digite o salario dos funcionarios");
+
+	for (int i = 1; i < 11; i++){
+		printf("Funcionario %d: ", i);
+		scanf("%f", &salario);
+		soma_salario += salario;
+	}
+
+	media = soma_salario / 10;
+
+	printf("A media dos salarios e R$%.2f", media);
+	
 	return 0;
 }
