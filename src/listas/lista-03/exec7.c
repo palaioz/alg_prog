c76e20d34a5f360906c8056ad47e97d723333eef
palaioz <antonio.GOsTI@outlook.com>
2026-05-18 13:57:13 -0300

    listas 3 e 3b

--- src/listas/lista-03/exec7.c
+++ src/listas/lista-03/exec7.c
@@ -8,9 +8,28 @@ deve exibir o resultado das potências de 2^n, tal que 2 <= n <= 5. Utilize a fu
 #include <math.h>
 
 int main(){
+	double base;
+	int exp_inicio, exp_fim;
 
-	printf("Hello World\n");
-	float res = pow(2, 2);
-	printf("%.2f", res);
+	printf("\nDigite o numero base: ");
+	scanf("%lf", &base);
+
+	printf("\nDigite o inicio do intervalo de expoentes: ");
+	scanf("%d", &exp_inicio);
+
+	printf("\nDigite o fim do intervalo de expoentes: ");
+	scanf("%d", &exp_fim);
+
+	printf("\nResultados das potencias de %.2lf^n, tal que %d <= n <= %d:\n", base, exp_inicio, exp_fim);
+	printf("-----------------------------------------------------\n");
+
+	for (int n = exp_inicio; n <= exp_fim; n++){
+		double resultado = pow(base, n);
+
+		printf("%.0f^%d = %.0f\n", base, n, resultado);
+	}
+
+	printf("------------------------------------------\n");
+
 	return 0;
 }
