24fa61f1a41043634e57ed15a9e14c15a1ab3917
palaioz <antonio.GoSTI@outlook.com>
2026-06-01 17:27:05 -0300

    Atualizar o exec3.c

--- src/listas/trabalho_01/exec3.c
+++ src/listas/trabalho_01/exec3.c
@@ -6,7 +6,34 @@ necessários para que Joaquim esteja mais alto que Paulo.
 */
 
 #include <stdio.h>
+#include <windows.h>
+#include <locale.h>
+
 int main(){
-	printf("Hello World");
-	return 0;
+    SetConsoleOutputCP(65001);
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    float alt_paulo = 1.30;
+    float alt_joaquim = 1.25;
+    
+    float cresc_paulo = 0.02;
+    float cresc_joaquim = 0.03;
+    
+    int anos = 0;
+
+    // ---------- PROCESSAMENTO
+    while (alt_joaquim <= alt_paulo) {
+        alt_paulo += cresc_paulo;
+        alt_joaquim += cresc_joaquim;
+        anos++;
+    }
+
+    // ---------- OUTPUT
+    printf("\n---------------- EVOLUÇÃO ----------------\n");
+    printf("Anos necessários: %d anos\n", anos);
+    printf("Altura final Paulo: %.2fm\n", alt_paulo);
+    printf("Altura final Joaquim: %.2fm\n", alt_joaquim);
+    printf("------------------------------------------\n");
+
+    return 0;
 }
