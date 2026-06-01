86aed7f4430a513a4f8f863a6fbb2d6e9dc1fe62
palaioz <antonio.GoSTI@outlook.com>
2026-06-01 17:28:00 -0300

    Atualizar o exec5.c

--- src/listas/trabalho_01/exec5.c
+++ src/listas/trabalho_01/exec5.c
@@ -13,7 +13,80 @@ O algoritmo para de ler dados quando o tipo de consumidor fornecido for ‘X’.
 */
 
 #include <stdio.h>
+#include <windows.h>
+#include <locale.h>
+
 int main(){
-	printf("Hello World");
-	return 0;
+    SetConsoleOutputCP(65001);
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    char tipo;
+    int codigo;
+    float kwh;
+
+    int cod_menor_r, cod_maior_i;
+    float menor_r, maior_i;
+    
+    float soma_geral = 0.0;
+    int total_clientes = 0;
+    
+    int flag_r = 0, flag_i = 0;
+
+    while (1) {
+        // ---------- INPUT
+        printf("\nTipo do consumidor [R / C / I] (ou X para encerrar): ");
+        scanf(" %c", &tipo);
+
+        if (tipo == 'X' || tipo == 'x') {
+            break;
+        }
+
+        printf("Código de identificação: ");
+        scanf("%d", &codigo);
+        printf("Consumo mensal (kWh): ");
+        scanf("%f", &kwh);
+
+        // ---------- PROCESSAMENTO
+        soma_geral += kwh;
+        total_clientes++;
+
+        // Regras para residencial
+        if (tipo == 'R' || tipo == 'r') {
+            if (flag_r == 0 || kwh < menor_r) {
+                menor_r = kwh;
+                cod_menor_r = codigo;
+                flag_r = 1;
+            }
+        }
+        // Regras para industrial
+        else if (tipo == 'I' || tipo == 'i') {
+            if (flag_i == 0 || kwh > maior_i) {
+                maior_i = kwh;
+                cod_maior_i = codigo;
+                flag_i = 1;
+            }
+        }
+    }
+
+    // ---------- OUTPUT
+    printf("\n================ RESULTADOS DA PESQUISA ================\n");
+    
+    if (flag_r) {
+        printf("Menor consumo residencial \t|\t Código: %d \t|\t Consumo: %.2f kWh\n", cod_menor_r, menor_r);
+    } else {
+        printf("Menor consumo residencial \t|\t Sem registros cadastrados.\n");
+    }
+
+    if (flag_i) {
+        printf("Maior consumo industrial  \t|\t Código: %d \t|\t Consumo: %.2f kWh\n", cod_maior_i, maior_i);
+    } else {
+        printf("Maior consumo industrial  \t|\t Sem registros cadastrados.\n");
+    }
+
+    if (total_clientes > 0) {
+        printf("Média geral de consumo da cidade: %.2f kWh\n", soma_geral / total_clientes);
+    }
+    printf("========================================================\n");
+
+    return 0;
 }
