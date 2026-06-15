81b493e1c0cf32b01e8585377c4579ecdb52f217
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-06-15 08:27:43 -0300

    .

--- src/listas/lista-04/exec7.c
+++ src/listas/lista-04/exec7.c
@@ -5,6 +5,28 @@ vetor. O produto escalar é obtido da seguinte forma:
 
 #include <stdio.h>
 int main(){
-	printf("Hello World");
+	float Avec[8], Bvec[8];
+	float Cvec[8];
+
+	for (int i = 0; i < 8; i++){
+		printf("\nDigite o elemento Avec[%d]: ", i);
+		scanf("%f", &Avec[i]);
+	}
+	
+	for (int j = 0; j < 8; j++){
+		printf("\nDigite o elemento Avec[%d]: ", j);
+		scanf("%f", &Bvec[j]);
+	}
+
+	for (int k = 0; k < 8; k++){
+		Cvec[k] = Avec[k] * Bvec[k];
+	}
+
+	printf("\n");
+
+	for (int l = 0; l < 8; l++){
+		printf("Cvec[%d] == %.2f\n", l, Cvec[l]);
+	}
+	
 	return 0;
 }
